820a54fb3e51df3d628748fcec3e1a20d6b0615e
HarryET <29015545+HarryET@users.noreply.github.com>
2022-07-23 16:59:53 +0100

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,7 @@ function App({ Component, pageProps }: AppProps) {
       <Head>
         <title>Harry Bairstow</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+        <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "6af68ce72bd74260a556425735a80a3c"}'></script>
       </Head>
       <Component {...pageProps} />
     </>
