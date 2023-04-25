d8a97aff4abc4a9de411b5287d5eaa85d893f353
Harry Bairstow <me@harryet.xyz>
2023-04-25 20:19:28 +0100

    feat: update intro styles

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 
 export const Intro: React.FC = () => {
     return (
-        <div className='w-full flex flex-col'>
-            <div className="mb-2">
+        <div className='w-full flex flex-row items-center space-x-4'>
+            <div>
                 <Image src="https://github.com/harryet.png" alt='My profile picture' width={72} height={72} className="shadow-md" />
             </div>
-            <h1 className="text-2xl font-bold sm:text-4xl">Harry Bairstow</h1>
-            <p className='text-sm text-gray-600 sm:text-md dark:text-gray-400'>A-Level Computer Science Student</p>
+            <div>
+                <h1 className="text-2xl font-bold sm:text-4xl">Harry Bairstow</h1>
+                <p className='text-sm text-gray-600 sm:text-md dark:text-gray-400'>A-Level Computer Science Student</p>
+            </div>
         </div>
     )
 }
