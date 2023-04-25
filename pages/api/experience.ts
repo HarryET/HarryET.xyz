b1417eb8cd24165c22a1d6b089bf2d07f7fc472c
Harry Bairstow <me@harryet.xyz>
2023-04-25 20:20:59 +0100

    chore: remove old `api`

--- pages/api/experience.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import experience from "../../data/experience.json";
-
-export default function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).json(experience)
-}
--- pages/api/papers.ts
+++
@@ -1,9 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import papers from "../../data/papers.json";
-
-export default function handler(
-  _req: NextApiRequest,
-  res: NextApiResponse
-) {
-  res.status(200).json(papers)
-}
