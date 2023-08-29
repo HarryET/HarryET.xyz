1d35c4066035b4ebd35177884c52b87de4bad086
Harry Bairstow <me@harryet.xyz>
2023-08-29 23:12:15 +0100

    feat: post views

--- pages/blog/[...post].tsx
+++ pages/blog/[...post].tsx
@@ -5,6 +5,8 @@ import yaml from 'js-yaml'
 import rehypeRaw from "rehype-raw";
 import { Post } from '../../components/sections/blog';
 import { JetBrains_Mono, Inter } from 'next/font/google';
+import { ReportView } from '../../components/trackView';
+import { Redis } from '@upstash/redis';
 
 const jb_mono = JetBrains_Mono({ subsets: ['latin'] })
 const inter = Inter({ subsets: ['latin'] })
@@ -30,6 +32,8 @@ export const getStaticPaths: GetStaticPaths<StaticParams> = () => {
 type Params = { post: string; contents: string; meta: PostMeta; view_count: number; }
 export type PostMeta = Omit<Post, "name">
 
+const redis = Redis.fromEnv();
+
 export const getStaticProps: GetStaticProps = async (context) => {
     if (!context.params) {
         throw new Error("No params, cannot render blog post.")
@@ -42,19 +46,23 @@ export const getStaticProps: GetStaticProps = async (context) => {
     const meta = yaml.load(rawMeta) as PostMeta;
     const contents = rawContents.split("---")[1];
 
+    const val = await redis.get(`pageviews:${process.env.NODE_ENV}:${post}`);
+
     return {
         props: {
             post,
             contents,
             meta,
-            view_count: 'unknown'
-        }
+            view_count: val ?? 0
+        },
+        revalidate: 60
     }
 }
 
-const BlogPost: NextPage<Params> = ({ contents, meta, view_count }) => {
+const BlogPost: NextPage<Params> = ({ post, contents, meta, view_count }) => {
     return (
         <div className={'w-full h-full flex flex-col justify-between'}>
+            <ReportView slug={post} />
             <div className={`px-12 pt-12 flex flex-col items-start space-y-12 text-gray-900 ${jb_mono.className}`}>
                 <div id="intro" className='space-y-4'>
                     <h1 className='text-4xl font-semibold'>Harry Bairstow</h1>
@@ -84,14 +92,18 @@ const BlogPost: NextPage<Params> = ({ contents, meta, view_count }) => {
                         </div>
                     )}
                     <div className='flex flex-col h-full justify-center gap-2 items-end'>
-                        <p className='text-gray-500'>Published on {new Date(meta.date).toLocaleDateString()}</p>
-                        <p className='text-gray-500'>{view_count} views</p>
-                        <div className='text-gray-500 flex flex-row items-center gap-2 hover:underline hover:cursor-pointer' onClick={() => navigator.share({ url: window.location.href, title: meta.title })}>
+                        <p className='text-gray-500'>Published on {new Date(meta.date).toLocaleDateString('en-GB')}</p>
+                        <p className='text-gray-500'>{view_count.toLocaleString('en-GB')} views</p>
+                        <div id='share-button' className='text-gray-500 flex flex-row items-center gap-2 hover:underline hover:cursor-pointer' onClick={() => navigator.share({ url: window.location.href, title: meta.title })}>
                             Share
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-4 h-4">
                                 <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 100 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186l9.566-5.314m-9.566 7.5l9.566 5.314m0 0a2.25 2.25 0 103.935 2.186 2.25 2.25 0 00-3.935-2.186zm0-12.814a2.25 2.25 0 103.933-2.185 2.25 2.25 0 00-3.933 2.185z" />
                             </svg>
                         </div>
+                        <script dangerouslySetInnerHTML={{ __html: `if (!navigator.share) {
+                                document.getElementById('share-button').style.display = 'none';
+                            }`}}>
+                        </script>
                     </div>
                 </div>
                 <ReactMarkdown
